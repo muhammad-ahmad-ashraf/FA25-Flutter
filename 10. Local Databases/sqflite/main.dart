0f886056f871baeadb031d0024d3cd88d391cc04
Jamil226 <jamil138.amin@gmail.com>
2025-11-02 09:20:00 +0300

    updated-repo

--- 10. Local Databases/sqflite/main.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:my_first_app/expenses.dart';
-
-void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: ExpensesScreen(),
-  ));
-}
